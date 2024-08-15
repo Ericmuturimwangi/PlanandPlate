84b0b3339cddc0bf0a87ea805bc9f49ef538de82
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-08-15 10:49:56 +0300

    planandplate added the port configuration

--- main/settings.py
+++ main/settings.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 if os.path.exists("env.py"):
     import env
 
